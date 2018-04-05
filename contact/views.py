7bf5f1568f7c378392a528eb8362ba4ac0bd7794
Troadio Tecson III <troadio.tecson@tekinbox.com>
2018-04-05 10:30:31 +0800

    added create functions to view.py

--- contact/views.py
+++ contact/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 from .models import Contact
 
 def view(request):
-
     contact_list = Contact.objects.all()
     return render(request, 'contact/index.html', { 'contact_list' : contact_list })
+
+def create(request):
+    return render(request,'contact/create.html')
+
+def create_post(request):
+    contact = Contact()
+    contact.name = request.POST['name']
+    contact.phone = request.POST['phone']
+    contact.save()
+    return render(request, 'contact/create.html')
